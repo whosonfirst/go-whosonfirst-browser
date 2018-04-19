61edf4f8434ace78ab675f414d7cb5dde82b591c
thisisaaronland <straup+github@gmail.com>
2018-04-19 11:56:28 -0700

    make geojson handler work, yeah?

--- http/geojson.go
+++ http/geojson.go
@@ -1,7 +1,6 @@
 package http
 
 import (
-	"encoding/json"
 	"github.com/whosonfirst/go-whosonfirst-readwrite/reader"
 	"github.com/whosonfirst/go-whosonfirst-static/utils"
 	gohttp "net/http"
@@ -18,17 +17,10 @@ func GeoJSONHandler(r reader.Reader) (gohttp.Handler, error) {
 			return
 		}
 
-		body, err := json.Marshal(f.Bytes())
-
-		if err != nil {
-			gohttp.Error(rsp, err.Error(), gohttp.StatusInternalServerError)
-			return
-		}
-
 		rsp.Header().Set("Content-Type", "application/json")
 		rsp.Header().Set("Access-Control-Allow-Origin", "*")
 
-		rsp.Write(body)
+		rsp.Write(f.Bytes())
 	}
 
 	h := gohttp.HandlerFunc(fn)
