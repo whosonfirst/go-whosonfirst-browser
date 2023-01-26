f570bdc53a2e62b6ea3111ec3efb97ed89c92c15
sfomuseumbot <sfomuseumbot@localhost>
2023-01-26 14:51:19 -0800

    snapshot: start refactoring templates a bit

--- templates/html/html.go
+++ templates/html/html.go
@@ -1,16 +1,8 @@
 package html
 
 import (
-	"context"
 	"embed"
-	sfom_html "github.com/sfomuseum/go-template/html"
-	"html/template"
 )
 
 //go:embed *.html
 var FS embed.FS
-
-func LoadTemplates(ctx context.Context) (*template.Template, error) {
-
-	return sfom_html.LoadTemplates(ctx, FS)
-}
