7514e74a81155584a290f9cb560e25f38a5a526b
sfomuseumbot <sfomuseumbot@localhost>
2023-01-31 15:41:14 -0800

    snapshot: wire in go-whosonfirst-validate-wasm; not implemented yet

--- static/javascript/whosonfirst.browser.create.init.js
+++ static/javascript/whosonfirst.browser.create.init.js
@@ -1,3 +1,13 @@
 window.addEventListener("load", function load(event){
-	whosonfirst.browser.create.init();
+
+    // whosonfirst.browser.create.init();
+    
+    whosonfirst.validate.feature.init()
+	       .then(rsp => {
+		   whosonfirst.browser.create.init();
+	       })
+	       .catch(err => {
+		   console.log("Failed to initialize validation code", err);
+	       });
+
 });
