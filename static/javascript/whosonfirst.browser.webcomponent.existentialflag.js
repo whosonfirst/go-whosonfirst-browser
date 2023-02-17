76fc236f67190c846936f7b48fcc9a93d0381771
sfomuseumbot <sfomuseumbot@localhost>
2023-02-16 16:12:49 -0800

    snapshot: working existential flag web component

--- static/javascript/whosonfirst.browser.webcomponent.existentialflag.js
+++ static/javascript/whosonfirst.browser.webcomponent.existentialflag.js
@@ -1,32 +1,38 @@
 class ExistentialFlag extends HTMLElement {
+    
     constructor() {
-	
 	super();
+    }
 
-	// Y U NO ATTRIBUTES???
-	var id = this.getAttribute("id");
-	console.log("ID", id);
+    connectedCallback() {
+	
+	var id = this.getAttribute("data-id");
 	
 	const shadow = this.attachShadow({mode: 'open'});
 	
-	// Create text node and add word count to it
 	var select = document.createElement('select');
+	select.setAttribute("class", "form-select wof-property");
 
-	const flags = {
+	const flags = [ "-1", "0" , "1" ];
+	
+	const labels = {
 	    "-1": "unknown",
 	    "0": "false",
 	    "1": "true"
 	};
 
 	for (var i in flags){
-
 	    var option = document.createElement("option");
-	    option.setAttribute("value", i);
-	    option.appendChild(document.createTextNode(flags[i]));
-
+	    option.setAttribute("value", flags[i]);
+	    option.appendChild(document.createTextNode(labels[flags[i]]));
 	    select.appendChild(option);
 	}
 
+	select.onchange = function(){
+	    var textarea = document.getElementById(id);
+	    textarea.value = parseInt(select.value);
+	};
+	
 	shadow.appendChild(select);
     }
 }
