c70f79eec7936b9d12da519f6db12eb19e236476
sfomuseumbot <sfomuseumbot@localhost>
2023-03-10 13:46:45 -0800

    wrap whosonfirst.webcomponent.placetype.js in onload event to ensure all deps are loaded

--- static/javascript/whosonfirst.webcomponent.placetype.js
+++ static/javascript/whosonfirst.webcomponent.placetype.js
@@ -1,75 +1,79 @@
-class WhosOnFirstPlacetype extends HTMLElement {
+window.addEventListener("load", function load(event){
     
-    constructor() {
-	super();
-    }
-
-    connectedCallback() {
-
-	var wasm_uri = whosonfirst.browser.uris.forCustomLabel("placetypes_wasm");
-	var _self = this;
-
-	// Once we have placetypes, replace textarea with select menu
+    class WhosOnFirstPlacetype extends HTMLElement {
 	
-	var placetypes_cb = function(placetypes){
-
-	    var count = placetypes.length;
-	    
-	    var id = _self.getAttribute("data-id");
+	constructor() {
+	    super();
+	}
 	
-	    const shadow = _self.attachShadow({mode: 'open'});
+	connectedCallback() {
 	    
-	    var select = document.createElement('select');
-	    select.setAttribute("class", "form-select wof-property");
-
-	    // I don't understand why this is necessary; it appears that shadow DOM elements don't get CSS?
-	    select.setAttribute("style", "display: block; padding: .3rem; font-size: 1rem; width: 100%;");
-
-	    var opt = document.createElement("option");
-	    select.appendChild(opt);
+	    var wasm_uri = whosonfirst.browser.uris.forCustomLabel("placetypes_wasm");
+	    var _self = this;
 	    
-	    for (var i=0; i < count; i++){
-		var pt = placetypes[i];
-		var name = pt["name"];
-
+	    // Once we have placetypes, replace textarea with select menu
+	    
+	    var placetypes_cb = function(placetypes){
+		
+		var count = placetypes.length;
+		
+		var id = _self.getAttribute("data-id");
+		
+		const shadow = _self.attachShadow({mode: 'open'});
+		
+		var select = document.createElement('select');
+		select.setAttribute("class", "form-select wof-property");
+		
+		// https://web.dev/shadowdom-v1/
+	        select.setAttribute("style", "display: block; padding: .3rem; font-size: 1rem; width: 100%;");
+		
 		var opt = document.createElement("option");
-		opt.value = name;
-		opt.appendChild(document.createTextNode(name));
 		select.appendChild(opt);
-	    }
-	    
-	    select.onchange = function(){
-		var textarea = document.getElementById(id);
-		textarea.value = select.value;
+		
+		for (var i=0; i < count; i++){
+		    var pt = placetypes[i];
+		    var name = pt["name"];
+		    
+		    var opt = document.createElement("option");
+		    opt.value = name;
+		    opt.appendChild(document.createTextNode(name));
+		    select.appendChild(opt);
+		}
+		
+		select.onchange = function(){
+		    var textarea = document.getElementById(id);
+		    textarea.value = select.value;
+		};
+		
+		shadow.appendChild(select);
 	    };
 	    
-	    shadow.appendChild(select);
-	};
-
-	// Once we've loaded the whosonfirst/go-whosonfirst-placetypes-wasm WASM binary
-	// fetch all the descendants for the 'planet' placetype
-	
-	var wasm_cb = function(){
-
-	    whosonfirst_placetypes_descendants("planet","common,common_optional,optional").then((data) => {
-		var placetypes = JSON.parse(data);
-		placetypes_cb(placetypes);
-	    }).catch((err)=> {
-		console.log(err);
+	    // Once we've loaded the whosonfirst/go-whosonfirst-placetypes-wasm WASM binary
+	    // fetch all the descendants for the 'planet' placetype
+	    
+	    var wasm_cb = function(){
+		
+		whosonfirst_placetypes_descendants("planet","common,common_optional,optional").then((data) => {
+		    var placetypes = JSON.parse(data);
+		    placetypes_cb(placetypes);
+		}).catch((err)=> {
+		    console.log(err);
+		});
+		
+	    };
+	    
+	    // Fetch the whosonfirst/go-whosonfirst-placetypes-wasm WASM binary
+	    
+	    sfomuseum.wasm.fetch(wasm_uri). then(() => {	    
+		wasm_cb();
+	    }).catch((err) => {
+		console.log("Failed to fetch ", wasm_uri, err);
 	    });
-			
-	};
+	    
+	}
+    };
 
-	// Fetch the whosonfirst/go-whosonfirst-placetypes-wasm WASM binary
+    // Define the new element
+    customElements.define('whosonfirst-placetype', WhosOnFirstPlacetype);
 
-	sfomuseum.wasm.fetch(wasm_uri). then(() => {	    
-	    wasm_cb();
-	}).catch((err) => {
-	    console.log("Failed to fetch ", wasm_uri, err);
-	});
-
-    }
-}
-
-// Define the new element
-customElements.define('whosonfirst-placetype', WhosOnFirstPlacetype);
+});
