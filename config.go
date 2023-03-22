2d4bdc30598ed7a04b688d595557cdebb0b54d97
thisisaaronland <thisisaaronland@localhost>
2023-03-22 15:39:59 -0700

    add <custom-> elements to header and footer templates

--- config.go
+++ config.go
@@ -14,106 +14,106 @@ import (
 )
 
 type Config struct {
-	AuthenticatorURI                  string   `json:"authenticator_uri"`
-	CacheURI                          string   `json:"cache_uri"`
-	CORSOrigins                       []string `json:"cors_origins,omitempty"`
-	CORSAllowCredentials              bool     `json:"cors_allow_credentials,omitempty"`
-	CustomChromeURI                   string   `json:"custom_chrome_uri,omitempty"`
-	CustomEditProperties              []string `json:"custom_edit_properties,omitempty"`
-	CustomEditValidationWasmDir       string   `json:"custom_edit_validation_wasm_dir"`
-	CustomEditValidationWasmPath      string   `json:"custom_edit_validation_wasm_path"`
-	DisableGeoJSON                    bool     `json:"disable_geojson,omitempty"`
-	DisableGeoJSONLD                  bool     `json:"disable_geojsonld,omitempty"`
-	DisableId                         bool     `json:"disable_id,omitempty"`
-	DisableIndex                      bool     `json:"disable_index,omitempty"`
-	DisableNavPlace                   bool     `json:"disable_navplace,omitempty"`
-	DisablePNG                        bool     `json:"disable_png,omitempty"`
-	DisableSearch                     bool     `json:"disable_search,omitempty"`
-	DisableSelect                     bool     `json:"disable_select"`
-	DisableSPR                        bool     `json:"disable_spr"`
-	DisableSVG                        bool     `json:"disable_svg,omitempty"`
-	DisableWebFinger                  bool     `json:"disable_webfinger"`
-	EnableAll                         bool     `json:"enable_all,omitempty"`
-	EnableCORS                        bool     `json:"enable_cors,omitempty"`
-	EnableCustomEditValidationWasm    bool     `json:"enable_custom_edit_validation_wasm"`
-	EnableData                        bool     `json:"enable_data,omitempty"`
-	EnableEdit                        bool     `json:"enable_edit,omitempty"`
-	EnableEditAPI                     bool     `json:"enable_edit_api,omitempty"`
-	EnableEditUI                      bool     `json:"enable_edit_ui,omitempty"`
-	EnableGeoJSON                     bool     `json:"enable_geojson,omitempty"`
-	EnableGeoJSONLD                   bool     `json:"enable_geojsonld,omitempty"`
-	EnableGraphics                    bool     `json:"enable_graphics"`
-	EnableHTML                        bool     `json:"enable_html,omitempty"`
-	EnableId                          bool     `json:"enable_id,omitempty"`
-	EnableIndex                       bool     `json:"enable_index,omitempty"`
-	EnableNavPlace                    bool     `json:"enable_navplace,omitempty"`
-	EnablePointInPolygon              bool     `json:"enable_point_in_polygon,omitempty"`
-	EnablePointInPolygonAPI           bool     `json:"enable_point_in_polygon_api,omitempty"`
-	EnablePNG                         bool     `json:"enable_png,omitempty"`
-	EnableSearch                      bool     `json:"enable_search,omitempty"`
-	EnableSearchAPI                   bool     `json:"enable_search_api,omitempty"`
-	EnableSelect                      bool     `json:"enable_select,omitempty"`
-	EnableSPR                         bool     `json:"enable_spr,omitempty"`
-	EnableSVG                         bool     `json:"enable_svg,omitempty"`
-	EnableWebFinger                   bool     `json:"enable_webfinger,omitempty"`
+	AuthenticatorURI               string   `json:"authenticator_uri"`
+	CacheURI                       string   `json:"cache_uri"`
+	CORSOrigins                    []string `json:"cors_origins,omitempty"`
+	CORSAllowCredentials           bool     `json:"cors_allow_credentials,omitempty"`
+	CustomChromeURI                string   `json:"custom_chrome_uri,omitempty"`
+	CustomEditProperties           []string `json:"custom_edit_properties,omitempty"`
+	CustomEditValidationWasmDir    string   `json:"custom_edit_validation_wasm_dir"`
+	CustomEditValidationWasmPath   string   `json:"custom_edit_validation_wasm_path"`
+	DisableGeoJSON                 bool     `json:"disable_geojson,omitempty"`
+	DisableGeoJSONLD               bool     `json:"disable_geojsonld,omitempty"`
+	DisableId                      bool     `json:"disable_id,omitempty"`
+	DisableIndex                   bool     `json:"disable_index,omitempty"`
+	DisableNavPlace                bool     `json:"disable_navplace,omitempty"`
+	DisablePNG                     bool     `json:"disable_png,omitempty"`
+	DisableSearch                  bool     `json:"disable_search,omitempty"`
+	DisableSelect                  bool     `json:"disable_select"`
+	DisableSPR                     bool     `json:"disable_spr"`
+	DisableSVG                     bool     `json:"disable_svg,omitempty"`
+	DisableWebFinger               bool     `json:"disable_webfinger"`
+	EnableAll                      bool     `json:"enable_all,omitempty"`
+	EnableCORS                     bool     `json:"enable_cors,omitempty"`
+	EnableCustomEditValidationWasm bool     `json:"enable_custom_edit_validation_wasm"`
+	EnableData                     bool     `json:"enable_data,omitempty"`
+	EnableEdit                     bool     `json:"enable_edit,omitempty"`
+	EnableEditAPI                  bool     `json:"enable_edit_api,omitempty"`
+	EnableEditUI                   bool     `json:"enable_edit_ui,omitempty"`
+	EnableGeoJSON                  bool     `json:"enable_geojson,omitempty"`
+	EnableGeoJSONLD                bool     `json:"enable_geojsonld,omitempty"`
+	EnableGraphics                 bool     `json:"enable_graphics"`
+	EnableHTML                     bool     `json:"enable_html,omitempty"`
+	EnableId                       bool     `json:"enable_id,omitempty"`
+	EnableIndex                    bool     `json:"enable_index,omitempty"`
+	EnableNavPlace                 bool     `json:"enable_navplace,omitempty"`
+	EnablePointInPolygon           bool     `json:"enable_point_in_polygon,omitempty"`
+	EnablePointInPolygonAPI        bool     `json:"enable_point_in_polygon_api,omitempty"`
+	EnablePNG                      bool     `json:"enable_png,omitempty"`
+	EnableSearch                   bool     `json:"enable_search,omitempty"`
+	EnableSearchAPI                bool     `json:"enable_search_api,omitempty"`
+	EnableSelect                   bool     `json:"enable_select,omitempty"`
+	EnableSPR                      bool     `json:"enable_spr,omitempty"`
+	EnableSVG                      bool     `json:"enable_svg,omitempty"`
+	EnableWebFinger                bool     `json:"enable_webfinger,omitempty"`
 	// A valid whosonfirst/go-whosonfirst-export/v2 URI used to export feature records that are updated.
-	ExporterURI                       string   `json:"exporter_uri,omitempty"`
-	JavaScriptAtEOF                   bool     `json:"javascript_at_eof"`
-	GitHubAccessTokenURI              string   `json:"github_accesstoken_uri,omitempty"`
-	GitHubReaderAccessTokenURI        string   `json:"github_reader_accesstoken_uri,omitempty"`
-	GitHubWriterAccessTokenURI        string   `json:"github_writer_accesstoken_uri,omitempty"`
-	MapProviderURI                    string   `json:"map_provider_uri"`
-	NavPlaceMaxFeatures               int      `json:"navplace_max_features,omitempty"`
-	PathCreateFeature                 string   `json:"path_create_feature,omitempty"`
-	PathCreateFeatureAPI              string   `json:"path_create_feature_api,omitempty"`
-	PathCessateFeatureAPI             string   `json:"path_cessate_feature_api,omitempty"`
-	PathDeprecateFeatureAPI           string   `json:"path_deprecate_feature_api,omitempty"`
-	PathEditGeometry                  string   `json:"path_edit_geometry,omitempty"`
-	PathEditGeometryAPI               string   `json:"path_edit_geometry_api,omitempty"`
-	PathGeoJSON                       string   `json:"path_geojson,omitempty"`
-	PathGeoJSONAlt                    []string `json:"path_geojson_alt,omitempty"`
-	PathGeoJSONLD                     string   `json:"path_geojsonld,omitempty"`
-	PathGeoJSONLDAlt                  []string `json:"path_geojsonld_alt,omitempty"`
-	PathIndex                         string   `json:"path_index,omitempty"`
-	PathId                            string   `json:"path_id,omitempty"`
-	PathNavPlace                      string   `json:"path_navplace,omitempty"`
-	PathNavPlaceAlt                   []string `json:"path_navplace_alt,omitempty"`
-	PathPing                          string   `json:"path_ping,omitempty"`
-	PathPNG                           string   `json:"path_png,omitempty"`
-	PathPNGAlt                        []string `json:"path_png_alt,omitempty"`
-	PathPointInPolygon                string   `json:"path_point_in_polygon,omitempty"`
-	PathPointInPolygonAPI             string   `json:"path_point_in_polygon_api,omitempty"`
-	PathSearch                        string   `json:"path_search,omitempty"`
-	PathSearchAPI                     string   `json:"path_search_api,omitempty"`
-	PathSelect                        string   `json:"path_select,omitempty"`
-	PathSelectAlt                     []string `json:"path_select_alt,omitempty"`
-	PathSPR                           string   `json:"path_spr,omitempty"`
-	PathSPRAlt                        []string `json:"path_spr,omitempty"`
-	PathSVG                           string   `json:"path_svg,omitempty"`
-	PathSVGAlt                        []string `json:"path_svg_alt,omitempty"`
-	PathWebFinger                     string   `json:"path_webfinger,omitempty"`
-	PathWebFingerAlt                  []string `json:"path_webfinger_alt,omitempty"`
-	PlacetypesDefinitionURI           string   `json:"placetypes_definition_uri,omitempty"`
+	ExporterURI                string   `json:"exporter_uri,omitempty"`
+	JavaScriptAtEOF            bool     `json:"javascript_at_eof"`
+	GitHubAccessTokenURI       string   `json:"github_accesstoken_uri,omitempty"`
+	GitHubReaderAccessTokenURI string   `json:"github_reader_accesstoken_uri,omitempty"`
+	GitHubWriterAccessTokenURI string   `json:"github_writer_accesstoken_uri,omitempty"`
+	MapProviderURI             string   `json:"map_provider_uri"`
+	NavPlaceMaxFeatures        int      `json:"navplace_max_features,omitempty"`
+	PathCreateFeature          string   `json:"path_create_feature,omitempty"`
+	PathCreateFeatureAPI       string   `json:"path_create_feature_api,omitempty"`
+	PathCessateFeatureAPI      string   `json:"path_cessate_feature_api,omitempty"`
+	PathDeprecateFeatureAPI    string   `json:"path_deprecate_feature_api,omitempty"`
+	PathEditGeometry           string   `json:"path_edit_geometry,omitempty"`
+	PathEditGeometryAPI        string   `json:"path_edit_geometry_api,omitempty"`
+	PathGeoJSON                string   `json:"path_geojson,omitempty"`
+	PathGeoJSONAlt             []string `json:"path_geojson_alt,omitempty"`
+	PathGeoJSONLD              string   `json:"path_geojsonld,omitempty"`
+	PathGeoJSONLDAlt           []string `json:"path_geojsonld_alt,omitempty"`
+	PathIndex                  string   `json:"path_index,omitempty"`
+	PathId                     string   `json:"path_id,omitempty"`
+	PathNavPlace               string   `json:"path_navplace,omitempty"`
+	PathNavPlaceAlt            []string `json:"path_navplace_alt,omitempty"`
+	PathPing                   string   `json:"path_ping,omitempty"`
+	PathPNG                    string   `json:"path_png,omitempty"`
+	PathPNGAlt                 []string `json:"path_png_alt,omitempty"`
+	PathPointInPolygon         string   `json:"path_point_in_polygon,omitempty"`
+	PathPointInPolygonAPI      string   `json:"path_point_in_polygon_api,omitempty"`
+	PathSearch                 string   `json:"path_search,omitempty"`
+	PathSearchAPI              string   `json:"path_search_api,omitempty"`
+	PathSelect                 string   `json:"path_select,omitempty"`
+	PathSelectAlt              []string `json:"path_select_alt,omitempty"`
+	PathSPR                    string   `json:"path_spr,omitempty"`
+	PathSPRAlt                 []string `json:"path_spr,omitempty"`
+	PathSVG                    string   `json:"path_svg,omitempty"`
+	PathSVGAlt                 []string `json:"path_svg_alt,omitempty"`
+	PathWebFinger              string   `json:"path_webfinger,omitempty"`
+	PathWebFingerAlt           []string `json:"path_webfinger_alt,omitempty"`
+	PlacetypesDefinitionURI    string   `json:"placetypes_definition_uri,omitempty"`
 	// An optional boolean flag signaling that the point-in-polygon hierarchy resolver should skip placetype filters. If
 	// true it is expected that your code will deal with that as needed elsewhere.
-	PointInPolygonSkipPlacetypeFilter bool     `json:"point_in_polygon_skip_placetype_filter"`
+	PointInPolygonSkipPlacetypeFilter bool `json:"point_in_polygon_skip_placetype_filter"`
 	// One or more valid whosonfirst/go-reader URIs
-	ReaderURIs                        []string `json:"reader_uris"`
+	ReaderURIs []string `json:"reader_uris"`
 	// An optional boolean flag signaling whether JavaScript and CSS assets should be combined and minified.
-	RollupAssets                      bool     `json:"rollup_assets"`
-	SearchDatabaseURI                 string   `json:"search_database_uri,omitempty"`
-	SelectPattern                     string   `json:"select_pattern,omitempty"`
+	RollupAssets      bool   `json:"rollup_assets"`
+	SearchDatabaseURI string `json:"search_database_uri,omitempty"`
+	SelectPattern     string `json:"select_pattern,omitempty"`
 	// A valid aaronland/go-http-server URI
-	ServerURI                         string   `json:"server_uri"`
+	ServerURI string `json:"server_uri"`
 	// A valid whosonfirst/go-whosonfirst-spatial.Database URI
-	SpatialDatabaseURI                string   `json:"spatial_database_uri,omitempty"`
-	URIPrefix                         string   `json:"uri_prefix,omitempty"`
+	SpatialDatabaseURI string `json:"spatial_database_uri,omitempty"`
+	URIPrefix          string `json:"uri_prefix,omitempty"`
 	// Enable verbose logging
-	Verbose                           bool     `json:"verbose,omitempty"`
+	Verbose bool `json:"verbose,omitempty"`
 	// The hostname to use for WebFinger output
-	WebFingerHostname                 string   `json:"webfinger_hostname,omitempty"`
+	WebFingerHostname string `json:"webfinger_hostname,omitempty"`
 	// One or more valid whosonfirst/go-writer/v3 URIs
-	WriterURIs                        []string `json:"writer_uris,omitempty"`
+	WriterURIs []string `json:"writer_uris,omitempty"`
 }
 
 func ConfigFromFlagSet(ctx context.Context, fs *flag.FlagSet) (*Config, error) {
@@ -127,7 +127,7 @@ func ConfigFromFlagSet(ctx context.Context, fs *flag.FlagSet) (*Config, error) {
 	}
 
 	fs.Set(provider.MapPrefixFlag, static_prefix)
-	
+
 	if config_uri != "" {
 
 		var cfg *Config
@@ -167,7 +167,7 @@ func ConfigFromFlagSet(ctx context.Context, fs *flag.FlagSet) (*Config, error) {
 	if err != nil {
 		return nil, fmt.Errorf("Failed to lookup %s flag, %w", provider.RollupAssetsFlag, err)
 	}
-	
+
 	cfg := &Config{
 		AuthenticatorURI:                  authenticator_uri,
 		CacheURI:                          cache_uri,
